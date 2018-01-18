a7725269d0376ba929bd1889a4d4c5fd34ec381d
swengeler <simon.wengeler@outlook.com>
2018-01-18 11:26:03 +0100

    Separated updating and pickling

--- analyses/pickle_experiments.py
+++ analyses/pickle_experiments.py
@@ -6,22 +6,14 @@ import time
 def load_all_files(load_few=False) -> list:
     filenames = get_all_filenames()
     if load_few and len(filenames) > 10:
-        filenames = filenames[0:100]
+        filenames = filenames[0:10]
     objects = [None for _ in range(0, len(filenames))]
 
     for idx, file in enumerate(filenames):
         if (idx + 1) % 100 == 0:
-            print("loaded {} files".format(idx + 1))
+            print("[INFO]: Loaded {} files.".format(idx + 1))
 
         experiment = import_experiment(file)
-        parameter_names = experiment['parameters']
-        parameter_names['epsilon_decay_episodes_required'] = parameter_names.pop('epsilon_decay_per_step')
-
-        rewards = experiment['rewards']
-        for i in range(len(rewards)):
-            for j in range(len(rewards[i])):
-                rewards[i][j] = rewards[i][j]/10
-
         objects[idx] = experiment
 
     return objects
@@ -33,18 +25,18 @@ def import_experiment(fn):
 
 
 def get_all_filenames():
-    path = "../data/"
-    return [path + file for file in os.listdir("../data/") if ".json" in file]
+    path = "../../data-new/"
+    return [path + file for file in os.listdir(path) if ".json" in file]
 
 
 if __name__ == '__main__':
-    print("loading experiments")
+    print("[INFO]: Loading experiments.")
     ct = time.time()
-    experiments = load_all_files(True)
-    print("finished loading in {} s".format(time.time() - ct))
+    experiments = load_all_files()
+    print("[INFO]: Finished loading in {}s.".format(time.time() - ct))
 
     obj = {}
     obj["experiments"] = experiments
 
-    with open("experiments.pickle", "wb") as f:
+    with open("../../experiments-new.pickle", "wb") as f:
         pickle.dump(experiments, f, protocol=pickle.HIGHEST_PROTOCOL)
---
+++ analyses/update_and_pickle.py
@@ -0,0 +1,50 @@
+import pickle
+import os
+import json
+import time
+
+def load_all_files(load_few=False) -> list:
+    filenames = get_all_filenames()
+    if load_few and len(filenames) > 10:
+        filenames = filenames[0:100]
+    objects = [None for _ in range(0, len(filenames))]
+
+    for idx, file in enumerate(filenames):
+        if (idx + 1) % 100 == 0:
+            print("loaded {} files".format(idx + 1))
+
+        experiment = import_experiment(file)
+        parameter_names = experiment['parameters']
+        parameter_names['epsilon_decay_episodes_required'] = parameter_names.pop('epsilon_decay_per_step')
+
+        rewards = experiment['rewards']
+        for i in range(len(rewards)):
+            for j in range(len(rewards[i])):
+                rewards[i][j] = rewards[i][j]/10
+
+        objects[idx] = experiment
+
+    return objects
+
+
+def import_experiment(fn):
+    with open(fn, 'r') as jsonf:
+        return json.load(jsonf)
+
+
+def get_all_filenames():
+    path = "../data/"
+    return [path + file for file in os.listdir("../data/") if ".json" in file]
+
+
+if __name__ == '__main__':
+    print("loading experiments")
+    ct = time.time()
+    experiments = load_all_files(True)
+    print("finished loading in {} s".format(time.time() - ct))
+
+    obj = {}
+    obj["experiments"] = experiments
+
+    with open("experiments.pickle", "wb") as f:
+        pickle.dump(experiments, f, protocol=pickle.HIGHEST_PROTOCOL)
